86f2011580ea044a48fde3da8fdb46a1fa9e2241
trishank96 <mukherjeetrish005@gmail.com>
2018-06-25 20:58:05 +0530

    Added login activity

---
+++ app/src/main/java/com/example/trishmuk/slashed/LoginActivity.kt
@@ -0,0 +1,21 @@
+package com.example.trishmuk.slashed
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun LoginClicked(view: View){
+
+    }
+
+    fun CreateUser(view: View){
+
+    }
+}
--- app/src/main/java/com/example/trishmuk/slashed/MainActivity.kt
+++ app/src/main/java/com/example/trishmuk/slashed/MainActivity.kt
@@ -1,13 +1,10 @@
 package com.example.trishmuk.slashed
 
+import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
@@ -38,13 +35,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun navlogin(view: View){
-
+        val loginIntent = Intent(this, LoginActivity::class.java)
+        startActivity(loginIntent)
     }
 
     fun navAddChannel(view: View){
 
     }
 
+    fun sendClicked(view: View){
+
+    }
 
 
 }
